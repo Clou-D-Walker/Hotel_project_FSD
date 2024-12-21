17f0e0742eb368db169a0d2fbb66c0d373d0e52b
Surya Sujit <35captainmarvel@gmail.com>
2024-12-21 21:08:39 +0530

    controller/hotel/limit query time

--- backend/controllers/hotel.js
+++ backend/controllers/hotel.js
@@ -45,7 +45,7 @@ export const getHotels = async (req, res, next) => {
     const hotels = await Hotel.find({
       ...others,
       cheapestPrice: { $gt: min | 1, $lt: max || 999 },
-    }).limit(req.query.limit);
+    }).limit(req.query.limit || 10);
     // res.json(hotels);
     res.status(200).json(hotels);
   } catch (err) {
